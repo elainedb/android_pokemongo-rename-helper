88c2382089aedaaf0747cfe9b7a66e45cf189c07
elaine <diasbatista.e@sfeir.com>
2019-03-31 07:40:32 -0300

    Add Firebase Analytics

--- app/src/main/java/dev/elainedb/pokemongorenamehelper/MainActivity.kt
+++ app/src/main/java/dev/elainedb/pokemongorenamehelper/MainActivity.kt
@@ -4,17 +4,19 @@ import android.app.Activity
 import android.os.Bundle
 import android.view.View
 import android.util.Log
+import com.google.firebase.analytics.FirebaseAnalytics
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : Activity() {
 
     private lateinit var uiModel: MainUi
     private lateinit var mNotificationHelper: NotificationHelper
+    private lateinit var firebaseAnalytics: FirebaseAnalytics
 
     companion object {
         private val TAG = MainActivity::class.java.simpleName
-
         private const val NOTIFICATION = 1300
+        private const val FAEVENT_SHOW_NOTIFICATION = "show_notification"
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,6 +24,7 @@ class MainActivity : Activity() {
         setContentView(R.layout.activity_main)
         mNotificationHelper = NotificationHelper(this)
         uiModel = MainUi()
+        firebaseAnalytics = FirebaseAnalytics.getInstance(this)
     }
 
     private fun sendNotification(id: Int) {
@@ -45,7 +48,10 @@ class MainActivity : Activity() {
 
         override fun onClick(view: View) {
             when (view.id) {
-                R.id.button -> sendNotification(NOTIFICATION)
+                R.id.button -> {
+                    sendNotification(NOTIFICATION)
+                    firebaseAnalytics.logEvent(FAEVENT_SHOW_NOTIFICATION, Bundle())
+                }
 //                R.id.dm_channel_settings_button -> goToNotificationChannelSettings(DM_CHANNEL)
 //                R.id.go_to_settings_button -> goToNotificationSettings()
                 else -> Log.e(TAG, getString(R.string.error_click))
